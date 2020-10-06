d4840689d0e97fc42cec5ad980a292639b7c7c18
louise <louise.decombe@laplateforme.io>
2020-10-06 08:52:20 -0400

    traitement données en cours

---
+++ class/traitement.php
@@ -0,0 +1,37 @@
+
+<?php
+
+class Todolist
+{
+
+  private $db;
+   private $connect;
+
+
+   public function __construct($db){
+
+       $this->db= $db;
+       $this->connect = $this->db->connect();
+   }
+
+
+public function ajout($data, ){
+
+
+
+
+public function afficher(){
+
+}
+
+public function supprimer(){
+
+}
+
+public function maj(){
+
+
+}
+
+};
+ ?>
