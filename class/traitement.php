d6fdc41e24a6f99a5c1596da4fca94bfa9837b2a
louise <louise.decombe@laplateforme.io>
2020-10-06 12:56:46 -0400

    avancement classe

--- class/traitement.php
+++ class/traitement.php
@@ -3,35 +3,34 @@
 
 class Todolist
 {
-
   private $db;
-   private $connect;
+  private $connect;
+  public function __construct($db){
+      $this->db= $db;
+      $this->connect = $this->db->connect();
+  }
+
+public function ajout($id_utilisateur, $nom, $create_at, $finished_at, $description, $statut, $assigned_to){
+  $query = $this->connect->prepare("INSERT INTO todo (id_utilisateur, nom, create_at, finished_at, description, statut, assigned_to) VALUES (:id_utilisateur,:nom,:create_at,description,statut,assigned_to)");
+  $query->bindParam(':id_utilisateur', $id_utilisateur);
+  $query->bindParam(':nom', $nom);
+  $query->bindParam(':create_at', $create_at);
+  $query->bindParam(':finished_at', $finished_at);
+  $query->bindParam(':description', $description);
+  $query->bindParam(':statut', $statut);
+  $query->bindParam(':assigned_to', $assigned_to);
+  $query->execute();
+}
 
 
-   public function __construct($db){
-
-       $this->db= $db;
-       $this->connect = $this->db->connect();
-   }
-
-
-public function ajout($data, ){
-
-
-
-
-public function afficher(){
+public function supprimer($id){
 
 }
 
-public function supprimer(){
-
-}
-
-public function maj(){
-
-
+public function maj($finished_at,$statut,$id){
 }
 
 };
+
+
  ?>
--- traitement/todolist_process.php
+++ traitement/todolist_process.php
@@ -1,20 +1,41 @@
 <?php
 
+
+//on inclut le fichier de config qui charge les classes
 require "../class/config.php";
 
+
+// si on souhaite ajouter une tâche à la todolist
 if(isset($_GET['ajouter'])){
 
+  if ($_POST["nom_tache"]) {
 
+      $id_utilisateur = $_SESSION['id_utilisateur'];
+      $create_at = date("Y-m-d H:i:s");
+      $nom = $_POST['nom_tache'];
+      $description = $_POST['description'];
+      $finished_at = null;
+      $statut = 'non';
+      $assigned_to = null;
+     $result = $todolist->ajout($id_utilisateur,$create_at,$nom,$description,$finished_at,$statut,$assigned_to);
+        echo  $result;
+  }
 }
 
+
+// si on souhaite surligner la tâche au clique pour dire qu'elle est terminée
 if(isset($_GET['maj'])){
-
-
 }
 
-
+//si on souhaite supprimer la tâche
 if(isset($_GET['supprimer'])){
 
+// si l'utilisateur clique sur la croix pour supprimer on récupère l'id de la tâche pour la supprimer
+  if($_POST["id"])
+  {
+
+    $result3 = $todolist->supprimer($id);
+}
 
 }
 
