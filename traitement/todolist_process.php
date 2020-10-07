dbf1ebb4542f15521d9e9ffc004cf762ce8e0f04
louise <louise.decombe@laplateforme.io>
2020-10-07 16:10:56 -0400

    avancement

--- traitement/todolist_process.php
+++ traitement/todolist_process.php
@@ -4,11 +4,8 @@
 //on inclut le fichier de config qui charge les classes
 require "../class/config.php";
 
-
 // si on souhaite ajouter une tâche à la todolist
 if (isset($_GET['ajouter'])) {
-
-
     if ($_GET["nom_tache"]) {
         $id_utilisateur = $_SESSION['id_utilisateur'];
         $nom = $_GET['nom_tache'];
@@ -18,31 +15,23 @@ if (isset($_GET['ajouter'])) {
         $description = $_GET['description'];
         $assigned_to = null;
         $result = $todolist->ajout($id_utilisateur, $nom, $statut, $create_at, $finished_at, $description, $assigned_to);
-        echo  $result;
 
-if($result = $todolist->ajout($id_utilisateur, $nom, $statut, $create_at, $finished_at, $description, $assigned_to)
-){
 
-         $task_list_id = $result->lastInsertId();
-
-         echo '<a href="#" class="list-group-item" id="list-group-item-'.$task_list_id.'" data-id="'.$task_list_id.'">'.$_POST["task_name"].' <span class="badge" data-id="'.$task_list_id.'">X</span></a>';
         }
-
-
     }
-}
+
+
 
 
 // si on souhaite surligner la tâche au clique pour dire qu'elle est terminée
 if (isset($_GET['maj'])) {
 
 // on sélectionn l'id
-  if($_GET["id"])
-  {
-$date_finished = date("Y-m-d H:i:s");
-$id = $_GET['id'];
-$statut= 'oui';
-  }
+    if ($_GET["id"]) {
+        $date_finished = date("Y-m-d H:i:s");
+        $id = $_GET['id'];
+        $statut= 'oui';
+    }
 }
 
 //si on souhaite supprimer la tâche
@@ -57,7 +46,6 @@ if (isset($_GET['supprimer'])) {
     ':id'  => $_GET['id']
    );
         $query = "DELETE FROM todo WHERE id = :id";
-
         $statement = $connect->prepare($query);
         if ($statement->execute($data)) {
             echo 'correctement supprimé';
